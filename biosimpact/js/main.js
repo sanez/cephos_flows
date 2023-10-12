75fe3169016bb5d0c5321a5a6638f8f3da048885
sanez <salazzari@gmail.com>
2023-10-12 14:44:05 +0200

    Update main.js

--- biosimpact/js/main.js
+++ biosimpact/js/main.js
@@ -41,7 +41,26 @@
     el.addEventListener('scroll', listener)
   }
 
-  
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#schedule .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
   /**
    * Scrolls to an element with header offset
    */
@@ -63,13 +82,13 @@
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select('#schedule')
+  let selectHeader = select('#header')
   if (selectHeader) {
     const headerScrolled = () => {
-      if (window.scrollY > 10) {
-        selectHeader.classList.add('schedule-scrolled')
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
       } else {
-        selectHeader.classList.remove('schedule-scrolled')
+        selectHeader.classList.remove('header-scrolled')
       }
     }
     window.addEventListener('load', headerScrolled)
@@ -82,7 +101,7 @@
   let backtotop = select('.back-to-top')
   if (backtotop) {
     const toggleBacktotop = () => {
-      if (window.scrollY > 10) {
+      if (window.scrollY > 100) {
         backtotop.classList.add('active')
       } else {
         backtotop.classList.remove('active')
