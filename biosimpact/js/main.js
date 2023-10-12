e1f147d4592079a272695d2849ce43c643bf14e2
sanez <salazzari@gmail.com>
2023-10-12 14:24:18 +0200

    Update main.js

--- biosimpact/js/main.js
+++ biosimpact/js/main.js
@@ -41,25 +41,7 @@
     el.addEventListener('scroll', listener)
   }
 
-  /**
-   * Navbar links active state on scroll
-   */
-  let navbarlinks = select('#schedule .scrollto', true)
-  const navbarlinksActive = () => {
-    let position = window.scrollY + 100
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
-      } else {
-        navbarlink.classList.remove('active')
-      }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+ 
 
   /**
    * Scrolls to an element with header offset
